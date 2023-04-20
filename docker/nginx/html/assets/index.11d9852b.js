80d41bd39b1c6c743750ce36f6981b6d8f5d46ff
pange <yplovecl@sina.com>
2023-04-20 22:59:40 +0800

    html

---
+++ docker/nginx/html/assets/index.11d9852b.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.653668d7.js";import{r as s,o as n,c as t,e as l,f as c}from"./index.d87ad0ed.js";const _={__name:"index",setup(u){const r=s("/prod-api/druid/login.html");return(p,e)=>(n(),t("div",null,[l(c(o),{src:r.value,"onUpdate:src":e[0]||(e[0]=a=>r.value=a)},null,8,["src"])]))}};export{_ as default};
---
+++ docker/nginx/html/assets/index.f16ec1c0.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./index.653668d7.js";import{r as o,o as t,j as l,f as e,B as p}from"./index.d87ad0ed.js";const _={__name:"index",setup(u){const r=o("/prod-api/swagger-ui/index.html");return(c,s)=>(t(),l(e(n),{src:e(r),"onUpdate:src":s[0]||(s[0]=a=>p(r)?r.value=a:null)},null,8,["src"]))}};export{_ as default};
